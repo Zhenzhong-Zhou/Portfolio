543da52a8e75c67386b95fd475b8682bbf28e78e
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-19 15:27:20 -0600

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,9 +14,34 @@ import {
 
 const Footer = () => {
 	return (
-		<div>
-			Footer
-		</div>
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Call</LinkTitle>
+					<LinkItem href={"tel: 306-000-000"}>306-000-000</LinkItem>
+				</LinkColumn>
+				<LinkColumn>
+					<LinkTitle>Email</LinkTitle>
+					<LinkItem href={"mailto: bob0823.zhou@gmail.com"}>bob.zhou@usask.ca</LinkItem>
+				</LinkColumn>
+			</LinkList>
+			<SocialIconsContainer>
+				<CompanyContainer>
+					<Slogan>Some slogan.</Slogan>
+				</CompanyContainer>
+				<SocialContainer>
+					<SocialIcons href={"https://github.com/Zhenzhong-Zhou"}>
+						<AiFillGithub size={"3rem"}/>
+					</SocialIcons>
+					<SocialIcons href={"https://www.linkedin.com/in/zhenzhong-zhou-7baa21192/"}>
+						<AiFillLinkedin size={"3rem"}/>
+					</SocialIcons>
+					<SocialIcons href={"https://instagram.com"}>
+						<AiFillInstagram size={"3rem"}/>
+					</SocialIcons>
+				</SocialContainer>
+			</SocialIconsContainer>
+		</FooterWrapper>
 	);
 };
 
