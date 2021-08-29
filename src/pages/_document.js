abc27469da25508b7144af28e5dfa3c128887fb1
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-29 00:26:14 -0600

    Add title and icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,6 +36,9 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
 						rel="stylesheet"/>
 				</Head>
+				<meta charSet="UTF-8"/>
+				<title>Zhenzhong Zhou's Portfolio</title>
+				<link rel="icon" href="/public/favicon.ico"/>
 				<body>
 				<Main/>
 				<NextScript/>
