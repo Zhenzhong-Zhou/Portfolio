34ca96303cbcb38a5f6d5b06260fd5038304d34e
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-11-10 21:50:29 -0600

    Update project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,9 @@
 export const projects = [
 	{
-		title: 'CMPT281',
-		description: "HTML, CSS, and JavaScript in 2018 Fall Term.",
+		title: 'No Milk',
+		description: "Using HTML, CSS, and JavaScript build a basic and simple static web in 2018 " +
+			"when I took course CMPT281. The web is called no milk and it is a basic simple shopping cart web that allows" +
+			"users to buy products locally",
 		image: '/images/cmpt281.png',
 		tags: ["HTML", "CSS", "JavaScript"],
 		source: 'https://github.com/Zhenzhong-Zhou/CMPT281_Project',
@@ -9,9 +11,10 @@ export const projects = [
 		id: 0,
 	},
 	{
-		title: 'CMPT350 - Project 2020',
-		description: "A full stack online web programming for general user to buy or sell products through the website, " +
-			"and it is using JavaScript, HTML and CSS for frontend, also using Nodejs and express for backend, and using MongoDB for database",
+		title: 'Tian Mao',
+		description: "Using JavaScript, EJS, HTML, CSS, Node.js, express and MongoDB build a full stack web programming " +
+			"in 2020 when I took course CPMT350. The app is called Tian Mao and it is a simple shopping cart app that " +
+			"allows users to buy or sell products through the website",
 		image: '/images/cmpt350_project.png',
 		tags: ['EJS', "HTML", "CSS", "Node.js", "Express", "MongoDB"],
 		source: 'https://github.com/Zhenzhong-Zhou/Movie_App',
@@ -19,28 +22,34 @@ export const projects = [
 		id: 1,
 	},
 	{
-		title: 'CMPT306 2020',
-		description: "2D RPG game",
-		image: '',
-		tags: ["C#", "Unity"],
-		source: 'https://github.com/Zhenzhong-Zhou/Movie_App',
-		visit: 'https://netflix-app-client.netlify.app/register',
+		title: 'Survival Game',
+		description: "Using C# and Unity build a 2D RPG top-down game. The game is call survival game " +
+			"and it was created by CMPT306 team in 2020. The game is a simple dungeon game " +
+			"that users have to survive and fight with curve enemies.",
+		image: '/images/SurvivalGame.png',
+		tags: ["C#", "Unity 2D"],
+		source: 'https://github.com/selinaw0327/CMPT306',
+		visit: 'https://github.com/selinaw0327/CMPT306',
 		id: 0,
 	},
 	{
 		title: 'Depth Of Hell',
-		description: "Depths of Hell is small boss-rush game developed by a team working (Rocket Studios) remotely from home. The Rocket Studios was created by CMPT406 group in 2021. \n" +
-			"\n" +
-			"The game features interesting boss battles combined with dynamic difficulty adjustment, thus making it easier or more difficult depending on how the player is performing. The goal is to gain a sense of progression, and insight into how the fights work. ",
+		description: "Depths of Hell is small boss-rush game developed by a team working " +
+			"(Rocket Studios) remotely from home. The Rocket Studios was created by CMPT406 group in 2021. \n" +
+			"The game features interesting boss battles combined with dynamic difficulty adjustment, " +
+			"thus making it easier or more difficult depending on how the player is performing. " +
+			"The goal is to gain a sense of progression, and insight into how the fights work. ",
 		image: '/images/depth-of-hell.png',
-		tags: ['Unity 2D', 'C#'],
+		tags: ['C#', 'Unity 2D'],
 		source: 'https://github.com/Rocket-Studios/BulletHell',
 		visit: 'https://foolmonkey.github.io/DepthsOfHellWeb/',
 		id: 3,
 	},
 	{
-		title: 'MERN Memories 2021',
-		description: "Using React, Node.js, Express & MongoDB build a Full Stack MERN Application. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+		title: 'MERN Memories',
+		description: "Using React, Node.js, Express & MongoDB build a Full Stack MERN Application in 2021. " +
+			"The app is called Memories and it is a simple social media app that " +
+			"allows users to post interesting events that happened in their lives. It is a self-studying project.",
 		image: '/images/memories.png',
 		tags: ['Mongo', 'Express', 'React', 'Node'],
 		source: 'https://github.com/Zhenzhong-Zhou/Memory_Project',
@@ -49,31 +58,34 @@ export const projects = [
 	},
 	{
 		title: 'Admin Dashboard',
-		description: "It is an admin panel design using React, Recharts, Material UI, and CSS. There are some widgets showing some sales information, new members and latest transactions. The analytic chart displays active users monthly.",
+		description: "It is an admin panel design using React, Recharts, Material UI, and CSS. " +
+			"Some widgets show some sales information, new members and the latest transactions. " +
+			"The analytic chart displays active users monthly. It is a self-studying project building in 2021.",
 		image: '/images/dashboard.png',
 		tags: ['React', 'JavaScript', "Recharts", "Material UI", "CSS"],
 		source: 'https://github.com/Zhenzhong-Zhou/Admin_Dashboard',
 		visit: 'https://backend-dashboard.netlify.app/',
 		id: 5,
 	},
-	{
-		title: 'NETFLIX',
-		description: "",
-		image: '/images/NETFLIX.png',
-		tags: ['React', 'JavaScript'],
-		source: 'https://github.com/Zhenzhong-Zhou/Movie_App',
-		visit: 'https://netflix-app-client.netlify.app/register',
-		id: 6,
-	},
-	{
-		title: 'E-Commerce',
-		description: "While building it I use many advanced React & JavaScript topics, and use Stripe for card transactions. And trust me, e-commerce applications are impressive.",
-		image: '/images/E-Commerce.png',
-		tags: ['React', 'JavaScript'],
-		source: "https://github.com/Zhenzhong-Zhou/E-Commerce",
-		visit: "https://e-commerce-store-practice.netlify.app/",
-		id: 7,
-	},
+	// {
+	// 	title: 'NETFLIX',
+	// 	description: "",
+	// 	image: '/images/NETFLIX.png',
+	// 	tags: ['React', 'JavaScript'],
+	// 	source: 'https://github.com/Zhenzhong-Zhou/Movie_App',
+	// 	visit: 'https://netflix-app-client.netlify.app/register',
+	// 	id: 6,
+	// },
+	// {
+	// 	title: 'E-Commerce',
+	// 	description: "While building it I use many advanced React & JavaScript topics, " +
+	// 		"and use Stripe for card transactions. And trust me, e-commerce applications are impressive.",
+	// 	image: '/images/E-Commerce.png',
+	// 	tags: ['React', 'JavaScript'],
+	// 	source: "https://github.com/Zhenzhong-Zhou/E-Commerce",
+	// 	visit: "https://e-commerce-store-practice.netlify.app/",
+	// 	id: 7,
+	// },
 ];
 
 export const timeLineData = [
