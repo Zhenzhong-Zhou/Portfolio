e097c6db20e702014f04f23925d7e879cecfb9b1
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-15 21:37:12 -0600

    Minor changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,8 +10,11 @@ const Hero = () => (
 				Zhenzhong Zhou's Portfolio
 			</SectionTitle>
 			<SectionText>
-				Bachelor of Science – Computer Science&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2016 – 2021.04<br/>
-				University of Saskatchewan, Saskatoon, SK<br/><br/>
+				Bachelor of Science – Computer Science<br/>
+				University of Saskatchewan &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2016 – 2021.04<br/><br/>
+				Diploma in Business Administration - Supply Chain Management<br/>
+				PSB College/Academy &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;2011 – 2013.07<br/><br/>
+
 				I am a graduated university student with qualities of a satisfied organization,
 				time management, critical thinking, self-studying, team-oriented and exceptional communication skills.
 			</SectionText>
